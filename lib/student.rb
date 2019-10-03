1b9665f64ef022c121eb2881e1bf38e1d5cf249b
Cody Rickman <cody@creativemindstudios.com>
2019-10-03 01:20:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,7 @@ require_relative '../lib/student'
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 
 class Student
+  attr_accessor :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize (name, grade, id = nil)

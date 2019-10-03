980a8fd5e560672fa26bab3e06573c8775117a36
Cody Rickman <cody@creativemindstudios.com>
2019-10-03 01:18:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,20 @@ DB = {:conn => SQLite3::Database.new("db/students.db")}
 class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
+    SQL = sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
   end
 
 end
